2c2104e03627d3329adfda2b02cd03d0014809e7
Fredohm <fredwaucquez@hotmail.fr>
2022-02-28 07:59:31 +0100

    convert csv file to dictionary

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pandas
 
-# TODO 1. Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
+# Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
+nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_dict = {}
+for (index, row) in nato_df.iterrows():
+    nato_dict.__setitem__(row['letter'], row['code'])
+
+print(nato_dict)
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 

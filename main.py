197a86c0d94066901b7b524b31b037aae3a0f7cc
Fredohm <fredwaucquez@hotmail.fr>
2022-03-06 15:20:00 +0100

    refactor with List and Dictionary Comprehension better use. RELEASE 1.0

--- main.py
+++ main.py
@@ -2,14 +2,9 @@ import pandas
 
 # Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
-nato_dict = {}
-for (index, row) in nato_df.iterrows():
-    nato_dict.__setitem__(row['letter'], row['code'])
+nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 
 # Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ")
-code_list = []
-
-for letter in word:
-    code_list += [code for key, code in nato_dict.items() if letter.upper() == str(key)]
+word = input("Enter a word: ").upper()
+code_list = [nato_dict[letter] for letter in word]
 print(code_list)

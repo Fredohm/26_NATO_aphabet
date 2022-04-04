944fd55ddefb750836e926811dfcc14b982ce679
Fredohm <fredwaucquez@hotmail.fr>
2022-04-04 06:34:20 +0200

    implement error handling for user input RELEASE 1.1

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ import pandas
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 
+
 # Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-code_list = [nato_dict[letter] for letter in word]
-print(code_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        code_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(code_list)
+
+
+generate_phonetic()
